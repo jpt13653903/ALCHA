5c8e0af3ef1ff762a8495d01c4b4063a0be92c14
John-Philip Taylor <jpt13653903@gmail.com>
2019-03-14 19:03:20 +0200

    Added clock groups to the SDC output

--- Source/BackEnd/SDC.cpp
+++ Source/BackEnd/SDC.cpp
@@ -59,6 +59,21 @@ void SDC::BuildClocks(){
     }
     // No recursion required -- all pins are top-level at this point
   }
+
+  // Create clock groups
+  Constraints += "\nset_clock_groups -asynchronous";
+
+  for(auto SymbolIterator  = Global.Symbols.begin();
+           SymbolIterator != Global.Symbols.end();
+           SymbolIterator++){
+    if(SymbolIterator->second->Type == BASE::TYPE::Pin){
+      auto Pin  = (PIN*)(SymbolIterator->second);
+      auto Freq = Pin->GetAttrib("frequency");
+      // Already checked for validity above, so no need to check again
+      if(Freq) Constraints += " \\\n  -group " + Pin->Name;
+    }
+  }
+  Constraints += "\n";
 }
 //------------------------------------------------------------------------------
 
